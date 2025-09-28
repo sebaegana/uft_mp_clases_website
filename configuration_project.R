a5747307de9b1801e3e916956bc6348d49799286
sebaegana <sebaegana@gmail.com>
2025-09-28 13:25:48 -0300

    Deploy page

---
+++ configuration_project.R
@@ -0,0 +1,15 @@
+install.packages(c("usethis","gitcreds","gh","gert"))
+
+gitcreds::gitcreds_set()   # paste the token when prompted
+
+gh::gh_whoami()            # should show your GitHub user
+usethis::git_sitrep()      # should no longer show the 401
+
+usethis::edit_r_environ()
+
+# 2) Make the first commit (creates the first branch)
+gert::git_add(".")
+gert::git_commit("Initial Quarto site")
+
+usethis::use_github()  # creates repo, sets remote, pushes
+
