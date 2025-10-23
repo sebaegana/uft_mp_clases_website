2b09d4753041a38bc6a192175da0b51db180f00d
sebaegana <sebaegana@gmail.com>
2025-10-22 23:31:11 -0300

    Update diapos

--- configuration_project.R
+++ configuration_project.R
@@ -1,11 +1,12 @@
 install.packages(c("usethis","gitcreds","gh","gert"))
 
-gitcreds::gitcreds_set()   # paste the token when prompted
-
 gh::gh_whoami()            # should show your GitHub user
 usethis::git_sitrep()      # should no longer show the 401
-
 usethis::edit_r_environ()
+
+## Para hacer primer commit y crear repo
+
+usethis::use_git()
 
 # 2) Make the first commit (creates the first branch)
 gert::git_add(".")
